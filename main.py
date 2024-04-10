fix bugs in distance matrix rmsd calculation

main.py
@@ -552,38 +552,36 @@ def main():
             mol = read_input.Molecule()
             read_input.Dataset(mol, size, init, space, input_dir, "txt")
 
-            # calculate distance matrix
-            mat_r = analysis.get_rij(mol.coords, size)
+            # calculate distance matrix for all structures
+            mat_r = analysis.get_rij(mol.coords, mol.coords.shape[1], size)
             keep_list = []
-            D_ij_cut = float(input("Enter D cut-off (Ang.) > "))
+            D_cut = float(input("Enter D cut-off (Ang.) > "))
             delete = np.full((size), False)
-            D = np.zeros((size, size))
             for i in range(size):
                 print(i)
                 for j in range(i):
                     if not delete[j]:
                         # calculate rmsd for structures i and j
-                        D[i, j] = analysis.D_rmsd(i, j, mat_r)
+                        D = analysis.D_rmsd(i, j, mat_r)
                         # if structures are too similar remove structure i
-                        if D[i, j] < D_ij_cut:
+                        if D < D_cut:
                             delete[i] = True
                 if not delete[i]:
                     keep_list.append(i)
 
             # for remaining structures calculate average D_ij
-            D_sum = 0
-            count = 0
-            for i in range(size):
-                if not delete[i]:
-                    for j in range(i):
-                        if not delete[j]:
-                            D_sum += D[i,j]
-                            count += 1
+            #D_sum = 0
+            #count = 0
+            #for i in range(size):
+            #    if not delete[i]:
+            #        for j in range(i):
+            #            if not delete[j]:
+            #                count += 1
 
             indices = np.array(keep_list)
             print("Number of structures remaining = ", indices.shape[0])
-            n_pairs = (indices.shape[0] * (indices.shape[0] - 1))/2
-            print("Mean D in new dataset = ", D_sum/n_pairs, "Angstrom")
+            #n_pairs = (indices.shape[0] * (indices.shape[0] - 1))/2
+            #print("Mean D in new dataset = ", D_sum/n_pairs, "Angstrom")
 
             new_energies = np.take(mol.energies, indices, axis=0)
             new_coords = np.take(mol.coords, indices, axis=0)

md.py
@@ -254,32 +254,39 @@ def simulate(simulation, system, force_field, output_dir, md_params, gro, top, m
         coords = simulation.context.getState(getPositions=True). \
             getPositions(asNumpy=True).in_units_of(angstrom)
 
-        # if doing distance matrix sampling decide whether to save structure
+        # distance matrix rmsd sampling
         if md_params["D_sample"]:
+            # get the distance matrix for the first structure and add to dataset arrau
             if i == 0:
-                mat_r = analysis.get_rij(coords, 1)
-                mat_d = mat_r
+                mat_d = analysis.get_rij(np.reshape(coords,
+                    (1,coords.shape[0],3)),coords.shape[0],1)
                 print_coords = True
-                size = 1
+                size = 1 # count number of structures in dataset
+                print(i, size)
 
             if i > D_start:
                 if (i % print_data) == 0:
-                    mat_r = analysis.get_rij(coords, 1)
+                    # get distance matrix for this structure and add to distance
+                    # matrix array for dataset
+                    mat_r = analysis.get_rij(np.reshape(coords,
+                        (1,coords.shape[0],3)),coords.shape[0],1)
                     test_mat_d = np.append(mat_d, mat_r, axis=0)
                     print_coords = True
 
-                    for j in range(mat_d.shape[0]):
+                    # calculate distance matrix RMSD for last vs all others
+                    for j in range(mat_d.shape[0]-1):
                         D = analysis.D_rmsd(-1, j, test_mat_d)
                         if D < D_cut:
                             print_coords = False
                             break
 
+                    # if last structure is sufficently different add to dataset
                     if print_coords:
                         mat_d = np.append(mat_d, mat_r, axis=0)
-                        size += 1
-                        #print(i, size)
+                        size += 1   # count number of structures in dataset
+                        print(i, size)
 
-        # print as normal if not using distance matrix or before D_start
+        # print as normal if not using distance matrix sampling
         elif (i % print_data) == 0 or i == 0:
             print_coords = True
 

network.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 import numpy as np
 import write_output, os, analysis, read_input
+# this is to stop the placeholder tensor bug in TF 2.12 - remove later
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 from tensorflow.keras.layers import Input, Dense, Layer
-from tensorflow.keras.models import Model
+from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.optimizers import Adam, RMSprop
 import shap
 
 # suppress printing of information messages
 tf.get_logger().setLevel("ERROR")
+print("Tensorflow version:", tf.__version__)
 
 class NuclearChargePairs(Layer):
     def __init__(self, _NC2, n_atoms, **kwargs):
@@ -215,6 +218,19 @@ class Network(object):
         train_charges = np.take(mol.charges, mol.train, axis=0)
         val_charges = np.take(mol.charges, mol.val, axis=0)
 
+        '''
+        pre_train = False
+        if pre_train:
+            train_energies = np.take(mol.orig_energies, mol.train, axis=0)
+            rel_energies = train_energies - min(train_energies)
+            nrfs = np.take(mol.mat_NRF, mol.train, axis=0)
+            model.fit(nrfs, rel_energies, epochs=200, verbose=2)
+            weights = model.layers[0].get_weights()[0]
+            print(nrfs.shape[0])
+            for i in range(nrfs.shape[1]):
+                print(mol.mat_i[i], mol.mat_j[i], weights[i][0])
+        '''
+
         # create arrays of nuclear charges for different sets
         train_atoms = np.tile(atoms, (len(train_coords), 1))
         val_atoms = np.tile(atoms, (len(val_coords), 1))
@@ -351,7 +367,6 @@ class Network(object):
             delimiter=" ", fmt="%.6f")
 
         # calculate electrostatic energy and compare to reference
-
         return None
 
 
@@ -386,6 +401,16 @@ class Network(object):
         # calculate scaled NRFs from coordinates and nuclear charges
         NRF_layer = CoordsToNRF(max_NRF, n_pairs, n_atoms, name='NRF_layer')\
                 ([coords_layer, nc_pairs_layer])
+
+        '''
+        pre_train = False
+        if pre_train:
+            model = Sequential()
+            model.add(Input(shape=(n_pairs,)))
+            model.add(Dense(1))
+            model.compile(loss="mse", optimizer=Adam(0.01), metrics=["mae"])
+            model.summary()
+        '''
 
         # define input layer as the NRFs
         connected_layer = NRF_layer
@@ -454,6 +479,9 @@ class Network(object):
 
 
     def feature_reduction(self, model, mol):
+        # create objection called an explainer - the object that takes as input
+        # the predict method and the training dataset
+        # in order to make SHAP model agnostic i
         explainer = shap.KernelExplainer(model.predict, mol.train)
         shap_values = explainer.shap_values(mol.test, nsamples=100)
         shap.summary_plot(shap_values, mol.test)